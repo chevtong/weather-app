5839d79eacb55616b1a4ea0a6d1c657486c2a623
chevtong <chevtongwingyan@gmail.com>
2020-12-01 14:56:48 +0100

    Fetch API

--- script.js
+++ script.js
@@ -4,10 +4,26 @@
 
         // console.log("clicked");
 
-        let city = document.querySelector("input").value;
+        
+        
+        getWeather();
+        
+        async function getWeather(){
+
+            const key = "5025dd46ae1a0b7d75dd6eda51911b7e";
+            
+            let city = document.querySelector("input").value;
+
+            const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+ city + '&appid=' + key);
+
+            const data = await response.json();
+
+            console.log(data);
+
+        //    const {}
+        }
 
         
-
 });
 
 
