1bc3a55737383f964132b40f5add13907f971730
chevtong <chevtongwingyan@gmail.com>
2020-12-01 16:11:33 +0100

    Add temperature on console

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
- document.querySelector("#submit").addEventListener("click", function(e){
+ document.querySelector("#submit").addEventListener("click", async function(e){
 
         e.preventDefault();
 
         // console.log("clicked");
-
-        
-        
-        getWeather();
-        
-        async function getWeather(){
-
             const key = "5025dd46ae1a0b7d75dd6eda51911b7e";
             
             let city = document.querySelector("input").value;
@@ -20,9 +13,25 @@
 
             console.log(data);
 
-        //    const {}
-        }
+            //get temperatures
+            var temperature = Math.round(parseFloat(data.main.temp)-273.15);
+            var highestTemp = Math.round(parseFloat(data.main.temp_max)-273.15); 
+            var lowestTemp = Math.round(parseFloat(data.main.temp_min)-273.15); 
 
+          
+            console.log("current temp: " + temperature);
+            console.log("highest temp: " + highestTemp);
+            console.log("lowest temp: " + lowestTemp);
+
+            //get current data
+            var sun = data.weather[0].description;
+
+            console.log(sun);
+
+          
+        
+        
+  
         
 });
 
