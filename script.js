8c9179581cb3a0bfa5d225f7441459b3e0de8030
chevtong <chevtongwingyan@gmail.com>
2020-12-01 14:04:35 +0100

    Add html

---
+++ script.js
@@ -0,0 +1,13 @@
+ document.querySelector("#submit").addEventListener("click", function(e){
+
+        e.preventDefault();
+
+        // console.log("clicked");
+
+        let city = document.querySelector("input").value;
+
+        
+
+});
+
+
