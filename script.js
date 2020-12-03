5cad0d701e8906ed0f1ab50b54f800133629ee62
chevtong <chevtongwingyan@gmail.com>
2020-12-03 16:28:44 +0100

    Delete duplicate functions, set variables for different cities

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
-
+//TODO: put the image.src as varibales, then both cities use the same functions 
  //TODO:  Remember the user choice on subsequent visits
  
 
- //EVENT HANDLERS
+//ALL VARIABLES:
+let cityTag; 
+let sunIcon;
+let currentTemp;
+let tempRange;
+let detailNum;
+let sunImage;
+let ctx;
+let chartColor;
+
+
+
+ //EVENT HANDLERS:
+ //default city
 window.onload = function() {
 
     getWeather("brussels");
-  };
+
+    cityTag = document.querySelector(".city");
+    sunIcon = document.querySelector(".sun");
+    currentTemp = document.querySelector(".current-temp");
+    tempRange = document.querySelector(".temp-highest");
+    detailNum = document.querySelector(".detail-num");
+    sunImage = document.querySelector("img");
+    ctx = document.getElementById('myLineChart').getContext('2d');
+};
 
 //first city btn
 document.querySelector("#submit").addEventListener("click", function(e){
@@ -14,6 +35,14 @@ document.querySelector("#submit").addEventListener("click", function(e){
     e.preventDefault();
 
     city = document.querySelector("input").value;
+    cityTag = document.querySelector(".city");
+    sunIcon = document.querySelector(".sun");
+    currentTemp = document.querySelector(".current-temp");
+    tempRange = document.querySelector(".temp-highest");
+    detailNum = document.querySelector(".detail-num");
+    sunImage = document.querySelector("img");
+    ctx = document.getElementById('myLineChart').getContext('2d');
+
     getWeather(city);
 
 });
@@ -21,12 +50,22 @@ document.querySelector("#submit").addEventListener("click", function(e){
 //plus city btn 
 document.querySelector("button.plus").addEventListener("click", function(){
 
+    //display the 2nd city container and delete button
     document.querySelector(".container2").style.display = "block";
     document.querySelector(".container2").style.display = "grid";
     document.querySelector("button.plus").style.display = "none";
     document.querySelector("button.delete").style.display = "block";
 
-    getWeather2("bangkok");
+    city = document.querySelector("#input2").value;
+    cityTag = document.querySelector(".city2"); 
+    sunIcon = document.querySelector(".sun2");
+    currentTemp = document.querySelector(".current-temp2");
+    tempRange = document.querySelector(".temp-highest2");
+    detailNum = document.querySelector(".detail-num2");
+    sunImage = document.querySelector("#image-city2");
+    ctx = document.getElementById('myLineChart2').getContext('2d');
+
+    getWeather("bangkok");
 
 });
 
@@ -43,8 +82,15 @@ document.querySelector("#submit2").addEventListener("click", function(e){
 
     e.preventDefault();
 
-    city2 = document.querySelector("#input2").value;
-    getWeather2(city2);
+    city = document.querySelector("#input2").value;
+    cityTag = document.querySelector(".city2"); 
+    sunIcon = document.querySelector(".sun2");
+    currentTemp = document.querySelector(".current-temp2");
+    tempRange = document.querySelector(".temp-highest2");
+    detailNum = document.querySelector(".detail-num2");
+    sunImage = document.querySelector("#image-city2");
+    ctx = document.getElementById('myLineChart2').getContext('2d');
+    getWeather(city);
 });
 
 
@@ -60,14 +106,13 @@ function getWeather(city){
     })
     .then((data) => {
             console.log(data);
-            display(data);
-            makeChart(data);
+             display(data);
+             makeChart(data);
         })
 };
 
 
 //display function
-let chartColor;
 function display(data){
 
     //get temperatures
@@ -82,33 +127,35 @@ function display(data){
     var humidity = data.data[0].rh;
     var windSpeed =  Math.round(data.data[0].wind_spd);
   
+    
+    
     //innerHTML wording display
-    document.querySelector(".city").innerHTML = data.city_name;
-    document.querySelector(".sun").innerHTML = description;
-    document.querySelector(".current-temp").innerHTML = temperature + "°c ";
-    document.querySelector(".temp-highest").innerHTML = `<i class="fas fa-caret-up"></i> ${highestTemp}°c   
-                                                          <i class="fas fa-caret-down"></i> ${lowestTemp}°c`;
-    document.querySelector(".detail-num").innerHTML = `${uv} <br>  ${precipation}% <br>
-                                                      ${humidity}% <br> ${windSpeed}km/h`;
+    cityTag.innerHTML = data.city_name;
+    sunIcon.innerHTML = description;
+    currentTemp.innerHTML = temperature + "°c ";
+    tempRange.innerHTML = `<i class="fas fa-caret-up"></i> ${highestTemp}°c   
+                           <i class="fas fa-caret-down"></i> ${lowestTemp}°c`;
+    detailNum.innerHTML = `${uv} <br>  ${precipation}% <br>
+                           ${humidity}% <br> ${windSpeed}km/h`;
 
     //show related img according to weather discription 
     if(temperature < "1"){
-        document.querySelector("img").src = "img/cold.png";
+        sunImage.src = "img/cold.png";
         chartColor = "#4793ff";
     } else if(description.includes("clouds")){
-        document.querySelector("img").src = "img/clouds.png";
+        sunImage.src = "img/clouds.png";
         chartColor = "#fcc28c";
     } else if(description.includes("thunderstorm")){
-        document.querySelector("img").src = "img/thunderstorm.png";
+        sunImage.src = "img/thunderstorm.png";
         chartColor = "#fcc28c";
     }  else if(description.includes("rain")){
-        document.querySelector("img").src = "img/rain.png";
+        sunImage.src = "img/rain.png";
         chartColor = "#515070";
     }  else if(description.includes("sun")){
-        document.querySelector("img").src = "img/sun.png";
+        sunImage.src = "img/sun.png";
         chartColor = "#fcc28c";
     }  else {
-        document.querySelector("img").src = "img/beach-sunset.png";
+        sunImage.src = "img/beach-sunset.png";
         chartColor = "#fcc28c";
     }   
 };
@@ -129,7 +176,7 @@ function makeChart(data){
     var date4 = data.data[4].valid_date;
     var date5 = data.data[5].valid_date;
 
-    var ctx = document.getElementById('myLineChart').getContext('2d');
+    
     var chart = new Chart(ctx, {
         // The type of chart we want to create
         type: 'line',
@@ -170,132 +217,4 @@ function makeChart(data){
         }
     });
 };
-
-
-//FUNCTIONS FOR CITY2
-//fetch API function
-function getWeather2(city2){
-
-    const key = "da9a51208d5e4403a9053883caf4d08d";
-        
-    fetch('https://api.weatherbit.io/v2.0/forecast/daily?city=' + city2 +'&key='+ key)
-    .then((resp) => {
-        return resp.json()
-    }) 
-    .then((data2) => {
-            console.log(data2);
-            display2(data2);
-            makeChart2(data2);
-        })
-}
-
-
-//display function
-let chartColor2;
-function display2(data2){
-
-    //get temperatures
-    var temperature2 = data2.data[0].temp;
-    var highestTemp2 = Math.round(data2.data[0].max_temp); 
-    var lowestTemp2 = Math.round(data2.data[0].min_temp); 
-    //get current weather discription
-    var description2 = data2.data[0].weather.description;
-    
-    //get weather details
-    var precipation2 = Math.round(data2.data[0].precip);
-    var uv2 = Math.round(data2.data[0].uv);
-    var humidity2 = data2.data[0].rh;
-    var windSpeed2 =  Math.round(data2.data[0].wind_spd);
-  
-    //innerHTML wording display
-    document.querySelector(".city2").innerHTML = data2.city_name;
-    document.querySelector(".sun2").innerHTML = description2;
-    document.querySelector(".current-temp2").innerHTML = temperature2 + "°c ";
-    document.querySelector(".temp-highest2").innerHTML = `<i class="fas fa-caret-up"></i> ${highestTemp2}°c   
-                                                          <i class="fas fa-caret-down"></i> ${lowestTemp2}°c`;
-    document.querySelector(".detail-num2").innerHTML = `${uv2} <br>  ${precipation2}% <br>
-                                                      ${humidity2}% <br> ${windSpeed2}km/h`;
-
-    //show related img according to weather discription 
-    if(temperature2 < "1"){
-        document.querySelector("#image-city2").src = "img/cold.png";
-        chartColor2 = "#4793ff";
-    } else if(description2.includes("clouds")){
-        document.querySelector("#image-city2").src = "img/clouds.png";
-        chartColor2 = "#fcc28c";
-    } else if(description2.includes("thunderstorm")){
-        document.querySelector("#image-city2").src = "img/thunderstorm.png";
-        chartColor2 = "#fcc28c";
-    }  else if(description2.includes("rain")){
-        document.querySelector("#image-city2").src = "img/rain.png";
-        chartColor2 = "#515070";
-    }  else if(description2.includes("sun")){
-        document.querySelector("#image-city2").src = "img/sun.png";
-        chartColor2 = "#fcc28c";
-    }  else {
-        document.querySelector("#image-city2").src = "img/beach-sunset.png";
-        chartColor2 = "#fcc28c";
-    }
-
-    
-}
-//chart function
-function makeChart2(data2){
-    //forcast for chart
-    var forcast12 = data2.data[1].temp;
-    var forcast22 = data2.data[2].temp;
-    var forcast32 = data2.data[3].temp;
-    var forcast42 = data2.data[4].temp;
-    var forcast52 = data2.data[5].temp;
-
-    //date for chart
-    var date12 = data2.data[1].valid_date;
-    var date22 = data2.data[2].valid_date;
-    var date32 = data2.data[3].valid_date;
-    var date42 = data2.data[4].valid_date;
-    var date52 = data2.data[5].valid_date;
-
-    var ctx2 = document.getElementById('myLineChart2').getContext('2d');
-    var chart2 = new Chart(ctx2, {
-        // The type of chart we want to create
-        type: 'line',
-    
-        // The data for our dataset
-        data: {
-            labels: [ date12 , date22, date32, date42, date52],
-            datasets: [{
-                label: 'TEMPERATURE FOR 5 DAYS',
-                backgroundColor: 'transparent',
-                borderColor: chartColor2,
-                data: [forcast12, forcast22, forcast32, forcast42, forcast52]
-            }]
-        },
-
-
-        // Configuration options go here
-        options: {
-            responsive: false,
-            scales: {
-                xAxes: [{
-                    gridLines: {
-                        drawOnChartArea: false
-                    }
-                }],
-
-
-                yAxes: [{
-                    
-                    ticks: {
-                        // Include a dollar sign in the ticks
-                        callback: function(value, index, values) {
-                            return value + '°C';
-                        },
-                        stepSize: 5
-                    }
-                }]
-            }
-        }
-    });
-};
-
 
